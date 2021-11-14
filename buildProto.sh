b422a01ff19525eedb28d35a46a1a54be8abe168
scottcalebc <sqq412@my.utsa.edu>
2021-11-13 19:27:22 -0600

    Update buildProto.sh
    Add subcommands and better usage text. Add ability to build out of source, not very convenient as all import statements of component python files would need to change

--- buildProto.sh
+++ buildProto.sh
@@ -3,45 +3,84 @@
 proj_path=$(pwd)
 
 build_proto() {
+
+    if [[ $# == 2 ]]; then
+        out_dir=$2
+    else 
+        out_dir="."
+    fi
+
     if [[ -f $1 ]]; then
         echo "Building proto file $1..."
-        python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. $1
+        python3 -m grpc_tools.protoc -I. --python_out=$out_dir --grpc_python_out=$out_dir $1
     else
         echo "Attempting to build all proto files in $1 directory"
-        for proto_file in "$1*.proto"; do
-            python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. $proto_file
+        for proto_file in $1/*.proto; do
+            echo "Building $(basename $proto_file)..."
+            python3 -m grpc_tools.protoc -I. --python_out=$out_dir --grpc_python_out=$out_dir $proto_file
         done
     fi
+    echo "Finished building"
 }
 
 usage() {
 
-    echo "Usage: ./buildProto.sh (proto_file|proto_path)"
-    echo "      proto - Name of the proto file you wish to copmile"
+    echo "Usage: ./buildProto.sh command"
+    echo "  Commands:"
+    echo "      build (proto_directory|proto_file) [out_directory]"
+    echo "          Args:"
+    echo "              proto_directory - Directory containin all proto files you want to compile"
+    echo "              proto_file - Path and name of proto file you wish to copmile"
+    echo "              out_directory - Directory where all compiled python files will be generated"
+    echo "      clean (directory)"
+    echo "          Args:"
+    echo "              directory - Directory containing all generated python files you want to remove"
     echo ""
+    echo "Examples:"
+    echo "  ./buildProto build interfaces"
+    echo "      This will build all the proto files within the interfaces directory"
+    echo "  ./buildProto build interfaces/chatserver.proto"
+    echo "      This will build the chatserver proto file"
+    echo "  ./buildProto clean interfaces"
+    echo "      This will remove all generated python files under interfaces directory"
     echo "Note:"
     echo "      Must be run within root directory of project"
-}
+    echo "      gRPC protoc will generated an interfaces directory under out_directory if compiling out of source"
 
-build() {
-
-    build_proto $1
-    echo "Finished building"
-    
-    
 }
 
 
 case $# in
     0)
-        echo "Setting python path (must be sourced) for project for easy imports to $proj_path..."
-        export PYTHONPATH="$proj_path:$PTYHONPATH"
-        ;;
-    1)
-        build $1
+        if [[ "$PYTHONPATH" == *"$proj_path"* ]]; then
+            echo "Python path already set for project root"
+        else 
+            echo "Setting python path (must be sourced) for project for easy imports to $proj_path..."
+            export PYTHONPATH="${PYTHONPATH}:$proj_path"
+        fi
         ;;
     *)
-        usage
+        case $1 in
+            "build")
+                # ignore the first two arguments (i.e. the file name and the "build" subcommand)
+                shift
+                echo "Cmd args: ${@}"
+                
+                build_proto "${@}"
+                ;;
+            "clean")
+                if [[ -d $2 ]]; then
+                    echo "Cleaning out $2 directory for generated python files"
+                    for py_file in $2/*pb2*.py; do 
+                        echo "Removing $(basename $py_file)"
+                        rm $py_file &> /dev/null
+                    done
+                fi
+                ;;
+            *)
+                usage
+                ;;
+        esac
     ;;
 esac
 
